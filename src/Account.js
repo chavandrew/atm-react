4e25546cc6af67e35d1a3f4ce7eb4d32d37b4310
Andrew Chavez <chavandrew@gmail.com>
2017-10-27 08:48:26 -0500

    hw

--- src/Account.js
+++ src/Account.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  class Account extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        balance: 0
+      }
+    }
+    handleDepositClick(e) {
+        e.preventDefault();
+        let amount = this.inputBox.value
+        let newBalance = this.state.balance + amount;
+        this.setState({
+          balance: newBalance
+        })
+        this.inputBox.value = '';
+      }
+
+      
+        let balanceClass = 'balance'
+        if (this.state.balance === 0) {
+          balanceClass += ' zero';
+        }
+}
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
+        <h2>Andrew Chavez</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
+        <input type="button" value="Deposit"  onClick={(e) => this.handleDepositClick(e)/>
         <input type="button" value="Withdraw" />
       </div>
     )
